9ce3b4d771c594ed8ae25d2b8614da82ec9ba3ba
shanshow <showdiango@gmail.com>
2017-10-31 23:26:10 +0800

    Reducing struct size in optimized
    Create a __PHONE_BOOK_DETAIL , then
     put some parameter from __PHONE_BOOK_ENTRY to here.
    In order to decrease the cache miss.

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,16 +1,32 @@
 #include <stdlib.h>
-
+#include <stdio.h>
+#include <string.h>
 #include "phonebook_opt.h"
 
 /* TODO: FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastName[], entry *pHead)
 {
     /* TODO: implement */
+
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0) {
+            printf(" %12s  is found!\n", lastName);
+            return pHead;
+        }
+        pHead = pHead->pNext;
+    }
+    printf(" %12s  is not found!\n", lastName);
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
     /* TODO: implement */
-    return NULL;
+
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+
+    return e;
 }
